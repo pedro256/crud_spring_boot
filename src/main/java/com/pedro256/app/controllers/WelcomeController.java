432ac2e5420be2f26634a33f17f86a043fdd273a
Pedro256 <pedroh93601@gmail.com>
2023-07-03 10:08:15 -0400

    exception handler

--- src/main/java/com/pedro256/app/controllers/WelcomeController.java
+++ src/main/java/com/pedro256/app/controllers/WelcomeController.java
@@ -1,5 +1,6 @@
 package com.pedro256.app.controllers;
 
+import com.pedro256.app.exceptions.BadRequestException;
 import com.pedro256.app.models.WelcomeVM;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +10,10 @@ public class WelcomeController {
 
     @GetMapping("/say")
     public WelcomeVM sayWelcome(@RequestParam(value = "name",defaultValue = "User")String name){
+
+        if(name.length()<4){
+            throw  new BadRequestException("Name deve conter mais de 3 caracteres");
+        }
         return  new WelcomeVM(1,name);
     }
 }
